84cc2031c814bee139fb62a3e7db2454be0f7523
Amanda Hansamali <amandahansamali18@gmail.com>
2025-03-18 18:12:58 +0530

    refactor: exceptionMapper classes

--- src/main/java/com/bookstore/util/exceptionMapper/CartNotFoundExceptionMapper.java
+++ src/main/java/com/bookstore/util/exceptionMapper/CartNotFoundExceptionMapper.java
@@ -1,4 +1,25 @@
 package com.bookstore.util.exceptionMapper;
 
-public class CartNotFoundExceptionMapper {
+import com.bookstore.util.exception.CartNotFoundException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class CartNotFoundExceptionMapper implements ExceptionMapper<CartNotFoundException> {
+    @Override
+    public Response toResponse(CartNotFoundException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Cart Not Found");
+        error.put("message", exception.getMessage());
+
+        return Response.status(Response.Status.NOT_FOUND)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(error)
+                .build();
+    }
 }
--- src/main/java/com/bookstore/util/exceptionMapper/InvalidInputExceptionMapper.java
+++ src/main/java/com/bookstore/util/exceptionMapper/InvalidInputExceptionMapper.java
@@ -1,4 +1,25 @@
 package com.bookstore.util.exceptionMapper;
 
-public class InvalidInputExceptionMapper {
+import com.bookstore.util.exception.InvalidInputException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class InvalidInputExceptionMapper implements ExceptionMapper<InvalidInputException> {
+    @Override
+    public Response toResponse(InvalidInputException exception) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", "Invalid Input");
+        error.put("message", exception.getMessage());
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .type(MediaType.APPLICATION_JSON)
+                .entity(error)
+                .build();
+    }
 }
